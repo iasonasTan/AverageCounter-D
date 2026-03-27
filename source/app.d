ce4aec09767bdf86fd807dcd54d40383633da219
Jason <iasonas.tan@gmail.com>
2026-03-27 12:37:08 +0200

    Initial Commit

---
+++ source/app.d
@@ -0,0 +1,101 @@
+import dlangui;
+import std.format : format;
+import std.conv;
+import std.stdio : writeln;
+
+mixin APP_ENTRY_POINT;
+
+final class UIManager {
+	private static UIManager instance;
+
+	public static UIManager get() {
+		if(instance is null) {
+			instance = new UIManager();
+		}
+		return instance;
+	}
+
+	// GUI
+	private Window window;
+	private EditLine inputWidget;
+	private TextWidget outputWidget;
+
+	// LOGIC
+	private Computer computer;
+
+	this() {
+		window = Platform.instance.createWindow(
+	    	UIString.fromRaw("Average Value Calculator."),
+	    	null,
+		);
+		VerticalLayout layout = new VerticalLayout();
+	    layout.margins = 20;
+
+	    auto label = new TextWidget(
+	    	null,
+	    	UIString.fromRaw("Enter values below: "),
+		);
+	    layout.addChild(label);
+
+	    inputWidget = new EditLine(null);
+	    layout.addChild(inputWidget);
+
+	    outputWidget = new TextWidget(
+	    	null,
+	    	UIString.fromRaw("Average: 0"),
+		);
+		layout.addChild(outputWidget);
+
+	    auto btn = new Button(
+	    	null,
+	    	UIString.fromRaw("Calculate!"),
+		);
+	    btn.click = delegate(Widget src) {
+	    	try {
+		    	computer.put(inputWidget.text.to!int);
+		    	string avgStr = computer.calculate().to!string;
+		    	outputWidget.text = UIString.fromRaw(avgStr);
+		    	outputWidget.textColor = Color.black;
+	    	} catch(Exception ignored) {
+	    		outputWidget.text = UIString.fromRaw("Error!");
+	    		outputWidget.textColor = Color.red;
+	    	}
+	    	return true;
+	    };
+	    layout.addChild(btn);
+
+	    window.mainWidget = layout;
+	}
+
+	public void start(Computer computer) {
+		this.computer = computer;
+		window.show();
+	}
+}
+
+interface Computer {
+	void put(int v);
+	int calculate();
+}
+
+final class AverageComputer : Computer {
+	private int counter = 0;
+	private int sum     = 0;
+
+	override public void put(int v) {
+		counter += 1;
+		sum     += v;
+	}
+
+	override public int calculate() {
+		if(counter == 0){
+			return 0;
+		}
+		return sum/counter;
+	}
+}
+
+extern (C) int UIAppMain(string[] args) {
+    UIManager.get().start(new AverageComputer());
+    return Platform.instance.enterMessageLoop();
+}
